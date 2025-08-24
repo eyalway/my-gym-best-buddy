59f3b2b81c5792de29819fbda8911ce8fc8cc88e
eyalway <eyalway@gmail.com>
2025-08-24 21:49:29 +0300

    Delete useKeepFullScreen.ts

--- useKeepFullScreen.ts
+++ useKeepFullScreen.ts
@@ -1,12 +1,1 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 
-export function useKeepFullScreen() {
-  const location = useLocation();
-
-  useEffect(() => {
-    if ((window.navigator as any).standalone) {
-      window.scrollTo(0, 0);
-    }
-  }, [location]);
-}
